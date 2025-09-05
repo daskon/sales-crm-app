201eea0bcc18f7ab03546feb9216e3744eb9a277
daskon <aselaforever@gmail.com>
2025-09-05 16:16:08 +0530

    cors added

--- api/src/server.ts
+++ api/src/server.ts
@@ -9,14 +9,25 @@ import cookieParser from "cookie-parser";
 dotenv.config();
 const app = express();
 
+const allowedOrigins = [process.env.FRONTEND_URL, "http://localhost:3000"];
+
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) === -1) {
+          const msg = `CORS does not allow access from the specified Origin.`;
+          return callback(new Error(msg), false);
+        }
+      return callback(null, true);
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
